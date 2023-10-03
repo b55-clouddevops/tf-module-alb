6e0aa8a99f360546b65317ed3a3b00d21141f5ad
CodingManoj <mvp2612@hotmail.com>
2023-10-02 23:17:22 -0400

    Git Auto Push

--- sg.tf
+++ sg.tf
@@ -1,5 +1,7 @@
 # Creates Security Group for Public-ALB
 resource "aws_security_group" "alb_public" {
+  count       = var.INTERNAL ? 0 : 1 
+
   name        = "roboshop-${var.ENV}-public-alb-sg"
   description = "Allows public traffic"
   vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
@@ -28,7 +30,9 @@ resource "aws_security_group" "alb_public" {
 
 
 # Creates Security Group for Private-ALB
-resource "aws_security_group" "alb_private" {
+resource "aws_security_group" "alb_private" { 
+  count       = var.INTERNAL ? 1 : 0   
+
   name        = "roboshop-${var.ENV}-private-alb-sg"
   description = "Allows Only private traffic"
   vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
